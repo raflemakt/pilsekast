443edb40582bf5eb7f334dff02005de38ab45c10
SiviKiwi <lauvdalsivert@gmail.com>
2022-10-21 14:58:51 +0200

    Add import arduino to file

--- src/configuration.h
+++ src/configuration.h
@@ -1,4 +1,5 @@
-// Konfigurasjon av aksesspunktsadresse.
+#include "arduino.h"
+//   Konfigurasjon av aksesspunktsadresse.
 //   Endre denne dersom en ny ESP32 med ny MAC-adresse brukes for
 //   aksesspunktet.
 const uint8_t AP_MAC_ADDRESS[] = {0x84, 0xCC, 0xA8, 0x61, 0x56, 0x88}; // Dette er TTGOESP32 med skeiv resetknapp
