b1e090b2d65d8a4fc1fca9819e942ff148997c16
raflemakt <raflemakt@hotmail.com>
2022-10-27 16:58:37 +0200

    Use esp_err_to_name to parse error codes

--- src/network/LocalNetworkInterface.cpp
+++ src/network/LocalNetworkInterface.cpp
@@ -49,42 +49,40 @@ void move_data_to_buffer(const uint8_t *data, const uint8_t size) {
 }
 
 void add_address_to_peers(const uint8_t *mac_addr) {
-        if (!esp_now_is_peer_exist(mac_addr)) {
+    if (!esp_now_is_peer_exist(mac_addr)) {
         memset(&peer_buffer, 0, sizeof(peer_buffer));
         peer_buffer.channel = LOCAL_NETWORK_CHANNEL;
         peer_buffer.encrypt = false;
         memcpy(&peer_buffer.peer_addr, mac_addr, ESP_NOW_ETH_ALEN);
 
-        Serial.print("  attempting to add peer: ");
-        Serial.println(Format::mac_addr_from_array(mac_addr));
         esp_now_status::peer_add = esp_now_add_peer(&peer_buffer);
 
-        switch (esp_now_status::peer_add) {
-            case (ESP_OK):
-                Serial.println("    peer successfully added");
-                break;
-            case (ESP_ERR_ESPNOW_NOT_INIT):
-                Serial.println("    failed, esp now not init");
-                break;
-            case (ESP_ERR_ESPNOW_FULL):
-                Serial.println("    failed, peer list full");
-                break;
-            case (ESP_ERR_ESPNOW_EXIST):
-                Serial.println("    failed, peer already exists");
-                break;
-        }
+        Serial.print("  attempting to add peer: ");
+        Serial.println(Format::mac_addr_from_array(mac_addr));;
+        Serial.print("    result: ");
+        Serial.println(esp_err_to_name(esp_now_status::peer_add));
+
         esp_now_get_peer_num(&peer_num);
 
         Serial.print("  number of peers: ");
         Serial.println(peer_num.total_num);
-    }else {
+    } else {
         Serial.println("  address exists in peer list");
     }
 }
 
 void esp_now_on_data_sent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  esp_now_status::last_send = status;
-  user_configured_send_cb();
+    esp_now_status::last_send = status;
+    Serial.print("  esp_now_send_status: ");
+    Serial.print(esp_now_status::last_send);
+
+    if (status != 0) {
+        Serial.println(" (fail)\n");
+        return;
+    } else {
+        Serial.println(" (success)");
+    }
+    user_configured_send_cb();
 }
 
 void esp_now_on_data_receive(const uint8_t *mac_addr, const uint8_t *incoming_data, int bytes_received) {
@@ -114,19 +112,18 @@ void initialize(){
 
     Serial.println("Esp Now initializing");
     Serial.print("  init result: ");
-    Serial.println(esp_now_status::init, BIN);
+    Serial.println(esp_err_to_name(esp_now_status::init));
 
     if (!IS_ACCESS_POINT) add_address_to_peers(AP_MAC_ADDRESS);
 
     esp_now_status::register_recv_callback = esp_now_register_recv_cb(esp_now_on_data_receive);
     esp_now_status::register_send_callback = esp_now_register_send_cb(esp_now_on_data_sent);
     
-    Serial.println("  esp_now callback functions registered");
+    Serial.println("  registering internal esp_now callbacks ");
     Serial.print("    send cb status: ");
-    Serial.print(esp_now_status::register_send_callback);
+    Serial.println(esp_err_to_name(esp_now_status::register_send_callback));
     Serial.print("    recv cb status: ");
-    Serial.print(esp_now_status::register_recv_callback);
-    Serial.println();
+    Serial.println(esp_err_to_name(esp_now_status::register_recv_callback));
 }
 
 void register_recv_callback(UserCallbackFunction callback_function) {
@@ -149,13 +146,6 @@ void send_binary_package(const uint8_t *peer_addr, const uint8_t *data, size_t l
     LocalNetworkInterface::transmission_size = len;
 
     Serial.print("  esp_now_send result: ");
-    Serial.print(result);
-
-    if (result != 0) {
-        Serial.println(" (fail)\n");
-        return;
-    } else {
-        Serial.println(" (success)");
-    }
+    Serial.println(esp_err_to_name(result));
 }
 }
