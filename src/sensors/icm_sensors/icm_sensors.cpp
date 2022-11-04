d2afa297986ce4c955f9b01416c6bd5b5ecaa0e0
William <william.skaftnes@gmail.com>
2022-11-04 14:55:56 +0100

    Ubidots lagt til

---
+++ src/sensors/icm_sensors/icm_sensors.cpp
@@ -0,0 +1,66 @@
+#include "ICM_20948.h"
+#define WIRE_PORT Wire
+#define AD0_VAL 1
+
+ICM_20948_I2C myICM;
+
+void ICMsetup()
+{
+  Serial.begin(115200);
+  while (!Serial)
+  {
+  };
+  WIRE_PORT.begin(21, 22);
+  WIRE_PORT.setClock(400000);
+  bool initialized = false;
+  while (!initialized)
+  {
+    myICM.begin(WIRE_PORT, AD0_VAL);
+    Serial.print(F("Initialization of the sensor returned: "));
+    Serial.println(myICM.statusString());
+    if (myICM.status != ICM_20948_Stat_Ok)
+    {
+      Serial.println("Trying again...");
+      delay(500);
+    }
+    else
+    {
+      initialized = true;
+    }
+  }
+}
+
+float absValuePrint(float x, float y, float z)
+{
+  return sqrt(sq(x) + sq(y) + sq(z));
+}
+
+void printAccVerdi(ICM_20948_I2C *sensor)
+{
+  float accVerdi = absValuePrint(9.81 / 1000 * (sensor->gyrX()), 9.81 / 1000 * (sensor->gyrY()), 9.81 / 1000 * (sensor->gyrZ()));
+  if (accVerdi > 0.1) //Filtrerer bort lave målinger til 0
+  {
+    Serial.print(accVerdi);
+
+  }
+  else {
+    Serial.print(float(0));
+  }
+  Serial.println(" m/s^2");
+}
+
+void ICMloop()
+{
+
+  if (myICM.dataReady())
+  {
+    myICM.getAGMT();
+    printAccVerdi(&myICM);
+    delay(100);
+  }
+  else
+  {
+    Serial.println("Waiting for data");
+    delay(100);
+  }
+}
