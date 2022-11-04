37e8f1a5b87f3d5d007494791a09972b28275151
Fredrik Kihl <fredrikkihl.01@gmail.com>
2022-11-04 18:09:32 +0100

    Added Registers to lightcontoller

---
+++ src/LedStrip/lightController.cpp
@@ -0,0 +1,32 @@
+#include "Arduino.h"
+#include "NeoPixelBus.h"
+#include <NeoPixelAnimator.h>
+#include "LedStripCustom.h"
+#include "network/Registers.h"
+
+uint8_t Red = oelkast_light_enveloped.color_red;
+uint8_t Green = oelkast_light_enveloped.color_green;
+uint8_t Blue = oelkast_light_enveloped.color_blue;
+RgbColor color = RgbColor(Red, Green, Blue);
+
+HslColor Actual_color = HslColor(color);
+float time_attack = oelkast_light_enveloped.env_attack_time;
+float time_duration = oelkast_light_enveloped.env_decay_time;
+float time_release = oelkast_light_enveloped.env_release_time;
+float duration = oelkast_light_enveloped.duration;
+float intensity = oelkast_light_enveloped.intensity;
+float actual_intensity = intensity * 0.01;
+float level = oelkast_light_enveloped.env_sustain_level;
+float function = oelkast_light_enveloped.packet_type;
+
+// void LedStripAdsrUpdate(float intensity, float time_attack, float time_duration, float level, float time_release, float duration);
+// uint8_t packet_type;
+// uint8_t intensity;
+// uint8_t color_red;
+// uint8_t color_green;
+// uint8_t color_blue;
+// uint8_t duration;
+// uint8_t env_attack_time;
+// uint8_t env_decay_time;
+// uint8_t env_sustain_level;
+// uint8_t env_release_time;
